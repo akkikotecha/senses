4ec412640b1bf7886a3ec51ab0b796eb47306f6f
Akkibudtech <106313176+Akkibudtech@users.noreply.github.com>
2023-07-03 10:14:35 +0530

    faq

--- src/app/faq/faq.component.ts
+++ src/app/faq/faq.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 declare var $: any;
+import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-faq',
   templateUrl: './faq.component.html',
   styleUrls: ['./faq.component.css']
 })
 export class FaqComponent {
+  constructor(private router: Router) { }
   ngOnInit(): void {
     // var loc = window.location;
 
@@ -28,5 +31,27 @@ export class FaqComponent {
       $('.logo img').css({ 'max-width': '170px' });
       $('.logo_style').attr('src', './assets/SENSES LOGO.svg');
     }, 200);
+  }
+
+  project_case(): void {
+    this.router.navigate(['insights_two']).then(() => {
+      // window.location.reload();
+      localStorage.setItem('project_case', 'project_case');
+      if (localStorage.getItem('project_case') == 'project_case') {
+        setTimeout(() => {
+          this.project_casescroll();
+        }, 1000);
+      }
+    });
+  }
+
+  project_casescroll() {
+    const element = document.getElementById('project_case');
+    if (element) {
+      const rect = element.getBoundingClientRect();
+      const offset = rect.top + window.scrollY;
+      window.scrollTo({ top: offset, behavior: 'smooth' });
+      localStorage.setItem('project_case', '');
+    }
   }
 }
