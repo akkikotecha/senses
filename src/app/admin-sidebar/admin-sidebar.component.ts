e212c8dc1b0cc9d3221642a6fdf4e31138aacba9
Ayush Yadav <infoayushyadav96@gmail.com>
2023-08-10 11:51:30 +0530

    Added changes in Side Bar

--- src/app/admin-sidebar/admin-sidebar.component.ts
+++ src/app/admin-sidebar/admin-sidebar.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-admin-sidebar',
@@ -6,5 +8,96 @@ import { Component } from '@angular/core';
   styleUrls: ['./admin-sidebar.component.css']
 })
 export class AdminSidebarComponent {
+  constructor(private router: Router) {
+
+
+
+  }
+
+  CategoryRoute(): void {
+    this.router.navigate(['admin_product'])
+      // .then(() => {
+      //   window.location.reload();
+      // });
+
+  }
+  SubCategoryRoute(): void {
+    this.router.navigate(['admin_sub_product'])
+      // .then(() => {
+      //   window.location.reload();
+      // });
+
+
+  }
+  AddProductRoute(): void {
+    this.router.navigate(['admin_product_detail'])
+      // .then(() => {
+      //   window.location.reload();
+      // });
+
+  }
+  ResourceRoute(): void {
+    this.router.navigate(['admin_resource_type'])
+      // .then(() => {
+      //   window.location.reload();
+      // });
+
+  }
+  SubResourceRoute(): void {
+    this.router.navigate(['admin_resource_sub_type'])
+      // .then(() => {
+      //   window.location.reload();
+      // });
+
+  }
+  AddResourceRoute(): void {
+    this.router.navigate(['admin_resource'])
+      // .then(() => {
+      //   window.location.reload();
+      // });
+
+  }
+  BannerRoute(): void {
+    this.router.navigate(['admin_home_banner'])
+      // .then(() => {
+      //   window.location.reload();
+      // });
+
+  }
+  FeaturedProductRoute(): void {
+    this.router.navigate(['admin_featured_product'])
+      // .then(() => {
+      //   window.location.reload();
+      // });
+
+  }
+  ProjectsRoute(): void {
+    this.router.navigate(['admin_featured_project'])
+      // .then(() => {
+      //   window.location.reload();
+      // });
+
+  }
+  BlogsRoute(): void {
+    this.router.navigate(['admin_blog_news'])
+      // .then(() => {
+      //   window.location.reload();
+      // });
+
+  }
+  InquiriesRoute(): void {
+    this.router.navigate(['admin_lets_talk'])
+      // .then(() => {
+      //   window.location.reload();
+      // });
+
+  }
+  MastersRoute(): void {
+    this.router.navigate(['admin_dashboard'])
+      // .then(() => {
+      //   window.location.reload();
+      // });
+
+  }
 
 }
